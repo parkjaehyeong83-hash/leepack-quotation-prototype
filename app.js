8c8c1579819293a490e6fb6845d9e0d8d4d94672
parkjaehyeong83-hash <parkjaehyeong83@gmail.com>
2026-09-09 10:58:04 +0900

    Finalize All Requests column widths

--- app.js
+++ app.js
@@ -1073,13 +1073,13 @@ const adminFilterHtml =
     ${adminFilterHtml}
       <table>
       <colgroup>
-  <col style="width:13%">
+ <col style="width:13%">
 <col style="width:10%">
+<col style="width:9%">
 <col style="width:7%">
-<col style="width:8%">
-<col style="width:6%">
-<col style="width:14%">
-<col style="width:17%">
+<col style="width:7%">
+<col style="width:13%">
+<col style="width:16%">
 <col style="width:10%">
 <col style="width:10%">
 <col style="width:5%">
