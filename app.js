f24f30d966633ba41f660daf3c8d5c0029ce2359
parkjaehyeong83-hash <parkjaehyeong83@gmail.com>
2026-09-09 11:12:24 +0900

    Give more space to View column

--- app.js
+++ app.js
@@ -1079,10 +1079,10 @@ const adminFilterHtml =
 <col style="width:7%">
 <col style="width:7%">
 <col style="width:13%">
-<col style="width:16%">
+<col style="width:14%">
 <col style="width:10%">
 <col style="width:9%">
-<col style="width:6%">
+<col style="width:8%">
 </colgroup>
         <thead>
           <tr>
